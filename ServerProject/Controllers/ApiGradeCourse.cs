57a6897ede373e340aa7969b338b9ae50e2e06d0
Donquijote <tungntd00606@fpt.edu.vn>
2019-01-05 22:53:22 +0700

    api-mark

---
+++ ServerProject/Controllers/ApiGradeCourse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServerProject.Models;
+
+namespace ServerProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiGradeCourse : ControllerBase
+    {
+        private readonly ServerProjectContext _context;
+
+        public ApiGradeCourse(ServerProjectContext context)
+        {
+            _context = context;
+        }
+//danh sach mon cua 1 lop
+        // GET: api/ApiGradeCourse
+        [HttpGet]
+        public IEnumerable<GradeCourse> GetGradeCourse(int gradeId)
+        {
+            return _context.GradeCourse.Where(a=> a.GradeId == gradeId);
+        }
+    }
+}
