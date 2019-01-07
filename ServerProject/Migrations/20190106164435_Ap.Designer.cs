66d16c535a0d4adf2ae2c942626f0df21eccd7bd
Donquijote <tungntd00606@fpt.edu.vn>
2019-01-07 09:46:55 +0700

    api list course

--- ServerProject/Migrations/20190106164435_Ap.Designer.cs
+++ ServerProject/Migrations/20190106164435_Ap.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServerProject.Models;
 
 namespace ServerProject.Migrations
 {
     [DbContext(typeof(ServerProjectContext))]
-    partial class ServerProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20190106164435_Ap")]
+    partial class Ap
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ServerProject/Models/ChangeStudentInformation.cs
+++ ServerProject/Models/ChangeStudentInformation.cs
@@ -7,13 +7,9 @@ namespace ServerProject.Models
 {
     public class ChangeStudentInformation
     {
-        public string Email { get; set; }
         public string newEmail { get; set; }
-        public string Address { get; set; }
         public string newAddress { get; set; }
-        public string Phone { get; set; }
-        public string newPhone { get; set; }
-        public string Avatar { get; set; }
+        public string newPhone { get; set; }        
         public string newAvatar { get; set; }
     }
 }
